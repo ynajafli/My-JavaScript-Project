c022288cc0aea824a4243a88f079071af1bbf720
ynajafli <ynajafli@outlook.com>
2024-08-07 12:09:56 -0600

    final product of the codecademy challegne

--- index.js
+++ index.js
@@ -1,42 +1,27 @@
-function generateRandomNumber(num) {
-    // Gets # from 0 -> num - 1
-    return Math.floor(Math.random() * num)
-  }
-  
-  const collectiveWisdom = {
+const generateRandom = num => Math.floor(Math.random() * num)
+
+
+
+const collectiveWisdom = {
     signInfo: ['star', 'moon', 'sun', 'comet'],
     fortuneOutput: ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
     advice: ['go out and eat', 'not read this', 'play more', 'trust no one']
   }
-  
-  // Store the 'wisdom' in an array
-  let personalWisdom = []
-  
-  // Iterate over the object
-  for(let prop in collectiveWisdom) {
-    let optionIdx = generateRandomNumber(collectiveWisdom[prop].length)
-  
-    // use the object's properties to customize the message being added to personalWisdom  
-    switch(prop) {
-      case 'signInfo':
-        personalWisdom.push(`Your sign right now is a "${collectiveWisdom[prop][optionIdx]}".`)
-        break
-      case 'fortuneOutput':
-        personalWisdom.push(`You are having: "${collectiveWisdom[prop][optionIdx]}".`)
-        break
-      case 'advice':
-        personalWisdom.push(`You should: "${collectiveWisdom[prop][optionIdx]}".`)
-        break
-      default:
-        personalWisdom.push('There is not enough info.')
+
+  for (let wisdom in collectiveWisdom) {
+    const index = generateRandom(collectiveWisdom[wisdom].length);
+
+    switch (wisdom) {
+        case 'signInfo':
+            console.log(`Your sign is: ${collectiveWisdom[wisdom][index]}`);
+            break;
+        case 'fortuneOutput':
+            console.log(`Your have: ${collectiveWisdom[wisdom][index]}`);
+            break;
+        case 'advice':
+            console.log(`You should: ${collectiveWisdom[wisdom][index]}`);
+            break;
     }
   }
-  
-  function formatWisdom(wisdom) {
-    // Add some ASCII here?
-    // Add in more symbols to the array? like emojies or what not?
-    const formatted = personalWisdom.join('\n')
-    console.log(formatted)
-  }
-  
-  formatWisdom(personalWisdom);
+
+
